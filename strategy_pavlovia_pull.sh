0c29458e7977f913cb10c03d8fdb972b1eaca194
daelsaid <daelsaid@gmail.com>
2020-09-26 12:40:55 -0700

    updated for os upgrade and pah changes

--- strategy_pavlovia_pull.sh
+++ strategy_pavlovia_pull.sh
@@ -3,8 +3,10 @@
 sa_date=`date "+%Y-%m-%d"`;
 todays_date=`date "+%m_%d_%Y"`;
 git pull;
-cd /Users/daelsaid/scratch/psychopy/strategy_assess_pavlovia/strategy_assessment_pilot/data;
+cd `pwd`/data;
 
-rclone mkdir stanford_box:/RAs_Telecommute_2020/MET_assessment_virtualization/SA_Pavlovia/raw_task_data/${todays_date};
+#/Users/daelsaid/scratch/psychopy/strategy_assess_pavlovia/strategy_assessment_pilot/data;
 
-for sa_csv in `ls *strategy*${sa_date}*`;do sa_file=`echo ${sa_csv}`; echo copying: `echo ${sa_file}`; rclone copy `ls ${sa_file}` stanford_box:/RAs_Telecommute_2020/MET_assessment_virtualization/SA_Pavlovia/raw_task_data/${todays_date};done
+rclone mkdir sbox:/RAs_Telecommute_2020/MET_assessment_virtualization/SA_Pavlovia/raw_task_data/${todays_date};
+
+for sa_csv in `ls *strategy*${sa_date}*`;do sa_file=`echo ${sa_csv}`; echo copying: `echo ${sa_file}`; rclone copy `ls ${sa_file}` sbox:/RAs_Telecommute_2020/MET_assessment_virtualization/SA_Pavlovia/raw_task_data/${todays_date};done
